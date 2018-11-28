5549d404dfa0c726adb5c1bef5d049762af7a6b8
Patricioerazo <patricio.erazo@hotmail.com>
2018-11-27 23:31:37 -0300

    Ultima Subida del 27-11

--- src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
+++ src/cpjlaboratoriofinal/CPJLaboratorioFinal.java
@@ -1,21 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package cpjlaboratoriofinal;
 
-/**
- *
- * @author Windows 10
- */
+import java.util.Scanner;
+        
 public class CPJLaboratorioFinal {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("1.- Iniciar Catalogo de Peliculas");
+        System.out.println("2.- Agregar Pelicula");
+        System.out.println("3.- Listar Peliculas");
+        System.out.println("4,. Buscar Pelicula");
+        System.out.println("0.- Salir");
+        
+        Scanner lector = new Scanner(System.in);
+        
     }
     
 }
